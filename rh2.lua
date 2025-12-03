ff03246ce209f60614aba9e1bb030638842fe150
nintendowithaswitch <anysocietytbh@gmail.com>
2025-12-03 03:29:49 -0600

    Update rh2.lua

--- rh2.lua
+++ rh2.lua
@@ -1,10 +1,9 @@
 task.wait(1)
 
 task.spawn(function()
-    while task.wait() do
-        local data = game.ReplicatedStorage.Remotes:WaitForChild("Data.RF", 10):InvokeServer("getData", game.Players.LocalPlayer.UserId, "Gameplay.Handler (Line 55)")
-        local slot = data.Slots[data["Player Info"].currentSlot]
-        local badges = slot.Badges
-        _G.quickdraw = badges.QuickDraw
-    end
+    local data = game.ReplicatedStorage.Remotes:WaitForChild("Data.RF", 10):InvokeServer("getData", game.Players.LocalPlayer.UserId, "Gameplay.Handler (Line 55)")
+    local slot = data.Slots[data["Player Info"].currentSlot]
+    local badges = slot.Badges
+    _G.quickdraw = badges.QuickDraw
+    warn(_G.quickdraw)
 end)
